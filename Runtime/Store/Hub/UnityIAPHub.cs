304161c7e4847d3776e750f14446e5249b9b5e68
Ivan <ivannikit16@gmail.com>
2022-05-13 13:19:39 +0300

    add UnityIAP package

--- Runtime/Store/Hub/UnityIAPHub.cs
+++ Runtime/Store/Hub/UnityIAPHub.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 namespace TeamZero.InAppPurchases
 {
-    public class UnityIAPHub : MonoBehaviour, IStoreHub, IPurchaseHub//, IStoreListener
+    public class UnityIAPHub : MonoBehaviour, IStoreHub, IPurchaseHub, IStoreListener
     {
         public static UnityIAPHub Create() => new ();
         
@@ -19,6 +20,26 @@ namespace TeamZero.InAppPurchases
         }
 
         public UniTask<bool> PurchaseAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void IStoreListener.OnInitializeFailed(InitializationFailureReason error)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        PurchaseProcessingResult IStoreListener.ProcessPurchase(PurchaseEventArgs purchaseEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void IStoreListener.OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void IStoreListener.OnInitialized(IStoreController controller, IExtensionProvider extensions)
         {
             throw new System.NotImplementedException();
         }
